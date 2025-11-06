588f59fd16863f9bc346d9b20aba331ac2c558da
Rafael Trevizoli <123047161+RafaelTrevizoli@users.noreply.github.com>
2025-11-06 15:34:37 -0300

    up

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import fetch from "node-fetch";
 
 const URL = "https://estornospp.up.railway.app/api/whatsapp/webhook/";
 
-// cron.schedule("30 12 * * *", async () => {
-cron.schedule("*/2 * * * *", async () => {
+// cron.schedule("30 12 * * *", async () => { // Executa todo dia às 12:30
+// cron.schedule("*/2 * * * *", async () => { // Executa a cada 2 minutos
+cron.schedule("0 10 * * *", async () => { // Executa todo dia às 10:00
+
   console.log("⏰ Executando alerta de estornos...");
   try {
     const res = await fetch(URL, { method: "POST" });
